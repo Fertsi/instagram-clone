c8ad7d71cd97717dba589785977ef871d1d6f2f5
Fertsi <100281472+Fertsi@users.noreply.github.com>
2023-10-24 13:24:36 +0300

    Finalized App

---
+++ data/users.js
@@ -0,0 +1,26 @@
+export const USERS = [
+    {
+      user: 'Fernando',
+      image: 'https://cdn-2.motorsport.com/images/mgl/6AEomeD6/s8/fernando-alonso-aston-martin-r-1.jpg',
+    },
+    {
+      user: 'Bobby',
+      image: 'https://randomuser.me/api/portraits/men/31.jpg',
+    },
+    {
+      user: 'Success',
+      image: 'https://randomuser.me/api/portraits/women/2.jpg',
+    },
+    {
+      user: 'Matt',
+      image: 'https://randomuser.me/api/portraits/men/60.jpg',
+    },
+    {
+      user: 'Lukas',
+      image: 'https://randomuser.me/api/portraits/men/59.jpg',
+    },
+    {
+      user: 'Sabrinah',
+      image: 'https://randomuser.me/api/portraits/women/29.jpg',
+    },
+  ]
